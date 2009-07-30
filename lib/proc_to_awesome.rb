27d5c7dc0136e939052eddd3006bdd4af65b89b4
Pat Maddox <pat.maddox@gmail.com>
2009-07-29 18:57:27 -0700

    added NilProc

--- lib/proc_to_awesome.rb
+++ lib/proc_to_awesome.rb
@@ -11,3 +11,26 @@ class Proc
     self[target]
   end
 end
+
+class NilProc
+  def ===(target)
+  end
+
+  def =~(target)
+  end
+
+  def match?(target)
+  end
+
+  class << self
+    private :new
+
+    def instance
+      @instance ||= new
+    end
+
+    def method_missing(m, *args, &block)
+      instance.send m, *args, &block
+    end
+  end
+end
--- spec/proc_to_awesome_spec.rb
+++ spec/proc_to_awesome_spec.rb
@@ -1,25 +1,34 @@
 require File.expand_path(File.dirname(__FILE__) + '/spec_helper')
 
-describe "Proc#===" do
-  it "should match if proc[target] returns truey" do
-    supa_proc = lambda {|target| target == 'cool!' }
-    supa_proc.should === 'cool!'
-    supa_proc.should_not === 'not cool!'
+describe Proc do
+  describe "===" do
+    it "should match if proc[target] returns truey" do
+      supa_proc = lambda {|target| target == 'cool!' }
+      supa_proc.should === 'cool!'
+      supa_proc.should_not === 'not cool!'
+    end
+  end
+
+  describe "=~" do
+    it "should match if proc[target] returns truey" do
+      supa_proc = lambda {|target| target == 'cool!' }
+      supa_proc.should =~ 'cool!'
+      supa_proc.should_not =~ 'not cool!'
+    end
+  end
+
+  describe "match?" do
+    it "should match if proc[target] returns truey" do
+      supa_proc = lambda {|target| target == 'cool!' }
+      supa_proc.should be_match('cool!')
+      supa_proc.should_not be_match('not cool!')
+    end
   end
 end
 
-describe "Proc#=~" do
-  it "should match if proc[target] returns truey" do
-    supa_proc = lambda {|target| target == 'cool!' }
-    supa_proc.should =~ 'cool!'
-    supa_proc.should_not =~ 'not cool!'
-  end
-end
-
-describe "Proc#match?" do
-  it "should match if proc[target] returns truey" do
-    supa_proc = lambda {|target| target == 'cool!' }
-    supa_proc.should be_match('cool!')
-    supa_proc.should_not be_match('not cool!')
-  end
+describe NilProc do
+  subject { NilProc }
+  it { should_not === :anything_ever }
+  it { should_not =~ :anything_ever }
+  it { should_not be_match(:anything_ever) }
 end
